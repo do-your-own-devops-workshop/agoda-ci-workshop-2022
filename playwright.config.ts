511608f80f6a17a67394214dbfdade43dc1b8cfc
Natchanon Nuntanirund <natchanon.nuntanirund@agoda.com>
2022-11-21 14:42:36 +0700

    playwright test only with chromium

--- playwright.config.ts
+++ playwright.config.ts
@@ -1,10 +1,12 @@
 import type { PlaywrightTestConfig } from '@playwright/test';
+
 const config: PlaywrightTestConfig = {
   testMatch: '**/e2e/*.spec.ts',
   use: {
+    browserName: 'chromium',
+    channel: 'chrome',
     viewport: { width: 1280, height: 720 },
-    ignoreHTTPSErrors: true,
-    video: 'on-first-retry',
   },
 };
+
 export default config;
