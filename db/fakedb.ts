ff661a0c147df42dd0ccd862a2f71744acd98007
kruengpratee <kavin.ruengprateepsang@agoda.com>
2022-11-22 14:38:53 +0700

    Add new hotel

--- db/fakedb.ts
+++ db/fakedb.ts
@@ -10,13 +10,13 @@ export const PropertyDb: PropertySearchResult[] = [
     free_cancellation: true,
   },
   {
-    name: 'aaaaaaa',
+    name: 'My hotel',
     image: '/biden.jpg',
-    score: 8.9,
-    price: 20,
+    score: 10,
+    price: 1000,
     discount: 0,
-    nbr_of_reviews: 20,
-    additional_label: 'WRITE ANYTHING HERE',
+    nbr_of_reviews: 40482,
+    additional_label: 'YAY',
     free_cancellation: true,
   },
 ];
