7737c2fc62029a80e4a7cbfdf70fbef274c47178
fernnicx <65396984+fernnicx@users.noreply.github.com>
2022-11-23 19:23:19 +0700

    Update fakedb.ts

--- db/fakedb.ts
+++ db/fakedb.ts
@@ -19,4 +19,14 @@ export const PropertyDb: PropertySearchResult[] = [
     additional_label: 'WRITE ANYTHING HERE',
     free_cancellation: true,
   },
+  {
+    name: 'fernfern',
+    image: '/biden.jpg',
+    score: 10,
+    price: 0,
+    discount: 0,
+    nbr_of_reviews: 20,
+    additional_label: 'WRITE ANYTHING HERE',
+    free_cancellation: true,
+  },
 ];
