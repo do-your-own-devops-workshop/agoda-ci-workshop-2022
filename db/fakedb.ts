00b1bada0efb645ee4330fbd93e7f7c5798f4273
Poonyavee Wongwisetsuk <pupshi@Poonyavees-MacBook-Air.local>
2022-11-23 19:18:58 +0700

    something

--- db/fakedb.ts
+++ db/fakedb.ts
@@ -19,4 +19,14 @@ export const PropertyDb: PropertySearchResult[] = [
     additional_label: 'WRITE ANYTHING HERE',
     free_cancellation: true,
   },
+  {
+    name: 'chip-hiw',
+    image: '/biden.jpg',
+    score: 8.9,
+    price: 20,
+    discount: 0,
+    nbr_of_reviews: 20,
+    additional_label: 'WRITE ANYTHING HERE',
+    free_cancellation: true,
+  },
 ];
